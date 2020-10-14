6c01f65888eac2b15b849dde71d1669cc63d994b
Brian Jacobs <brian.jacobs@natgeo.com>
2020-10-14 10:13:54 -0400

    add install script

---
+++ scripts/installIntoProject.sh
@@ -0,0 +1,8 @@
+parentdir=$(dirname `pwd`)
+
+if [ "$parentdir" = "node_modules" ]; then
+	cp -R src/ngm-assets/img/* ../../src/ngm-assets/img && \
+	cp ../mapbox-gl/dist/mapbox-gl.css ../../src/sass/mapbox-gl.scss && \
+	cp src/sass/base.scss ../../src/sass/ng-mapbox-locator.scss
+fi
+

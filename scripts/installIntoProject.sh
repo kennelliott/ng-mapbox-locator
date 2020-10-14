5337cb46a23cda2d230de72b0fa07573f173b976
Brian Jacobs <brian.jacobs@natgeo.com>
2020-10-14 10:19:43 -0400

    update script, fix illegal mapboxgl assignment

--- scripts/installIntoProject.sh
+++ scripts/installIntoProject.sh
@@ -4,5 +4,7 @@ if [ "$parentdir" = "node_modules" ]; then
 	cp -R src/ngm-assets/img/* ../../src/ngm-assets/img && \
 	cp ../mapbox-gl/dist/mapbox-gl.css ../../src/sass/mapbox-gl.scss && \
 	cp src/sass/base.scss ../../src/sass/ng-mapbox-locator.scss
+else
+    echo "parent dir $parentdir"
 fi
 

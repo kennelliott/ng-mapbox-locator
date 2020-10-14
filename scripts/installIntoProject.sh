db662090952b576af726dc281f8c1e39cbf1724c
Brian Jacobs <brian.jacobs@natgeo.com>
2020-10-14 11:09:30 -0400

    fix script base path

--- scripts/installIntoProject.sh
+++ scripts/installIntoProject.sh
@@ -1,6 +1,8 @@
+# this should be run in an npn postinstall script
+
 parentdir=$(dirname `pwd`)
 
-if [ "$parentdir" = "node_modules" ]; then
+if [ $(basename $parentdir) = "node_modules" ]; then
 	cp -R src/ngm-assets/img/* ../../src/ngm-assets/img && \
 	cp ../mapbox-gl/dist/mapbox-gl.css ../../src/sass/mapbox-gl.scss && \
 	cp src/sass/base.scss ../../src/sass/ng-mapbox-locator.scss

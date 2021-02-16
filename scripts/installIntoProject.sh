56a4abc55abbad1b94f26b86f99e38c40809edd5
Brian Jacobs <brian.jacobs@natgeo.com>
2021-02-16 14:14:51 -0500

    check for file when rerunning postinstall:

--- scripts/installIntoProject.sh
+++ scripts/installIntoProject.sh
@@ -6,7 +6,15 @@ if [ $(basename $parentdir) = "node_modules" ]; then
 	cp -R src/ngm-assets/img/* ../../src/ngm-assets/img && \
 	cp ../mapbox-gl/dist/mapbox-gl.css ../../src/sass/mapbox-gl.scss && \
 	cp src/sass/base.scss ../../src/sass/ng-mapbox-locator.scss && \
-	cp src/js/mapSettings.js ../../src/js/mapSettings.js
+
+	settingsFileDest=../../src/js/mapSettings.js
+	if [ ! -e "$settingsFileDest" ]; then
+		cp src/js/mapSettings.js $settingsFileDest
+	else
+		echo "$settingsFileDest exists, not overwriting"
+	fi
+
+	
 else
     echo "ngm-mapbox-locator: Unable to install local project assets"
 fi

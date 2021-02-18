4a4730ec43cd32409a66096cffa6ee228cf5538e
Brian Jacobs <brian.jacobs@natgeo.com>
2021-02-18 16:07:43 -0500

    fix default base

--- src/js/base.js
+++ src/js/base.js
@@ -1,16 +1,10 @@
-import "./lib/_project.js";
 /* YOUR GRAPHIC JS HERE */
 import NGLocator from './NGLocator'
-import mapSettings from './mapSettings'
 
 document.addEventListener('DOMContentLoaded', function() {
-// initiate your project after the document has loaded in
+    // initiate your project after the document has loaded in here!
+    this.els = document.querySelectorAll("[data-ng-locator-map]")
+    this.els.forEach(el => new NGLocator(el))
 
-    const locator = new NGLocator(document.querySelector(".ng-graphic-wrap"), mapSettings);
-    const map = locator.map
-
-    // wait for map readiness
-    map.on('load', () => {
-        // new layers go here e.g.: map.addLayer() 
-    })
 });
+
